29e6beea3aa535757f823e10c1b3c360da20ed43
Diyajain3 <diyajain3003@gmail.com>
2026-04-03 00:31:40 +0530

    Adding more content in my frontend

--- client/src/components/ProfessionalSummaryForm.jsx
+++ client/src/components/ProfessionalSummaryForm.jsx
@@ -1,25 +1,37 @@
-import React from 'react'
-import { Sparkles } from 'lucide-react'
-const ProfessionalSummaryForm = ({data, onChange, setResumeData}) => {
+import React from "react";
+import { Sparkles } from "lucide-react";
+const ProfessionalSummaryForm = ({ data, onChange, setResumeData }) => {
   return (
-    <div className='space-y-4'>
-      <div className='flex items-center justify-between'>
+    <div className="space-y-4">
+      <div className="flex items-center justify-between">
         <div>
-          <h3 className='flex items-center gap-2 text-lg font-semibold text-gray-900'>professional Summary</h3>
-          <p className='text-sm text-gray-500'>Add summary for your Resume here</p>
+          <h3 className="flex items-center gap-2 text-lg font-semibold text-gray-900">
+            Professional Summary
+          </h3>
+          <p className="text-sm text-gray-500">
+            Add summary for your Resume here
+          </p>
         </div>
-        <button className='flex items-center gap-2 px-3 py-1 text-sm bg-purple-100 text-purple-700 rounded hover:bg-purple-200 transition-colors disabled:opacity-50'>
-           <Sparkles className="size-4"> </Sparkles>
-            AI Enhance
-          
+        <button className="flex items-center gap-2 px-3 py-1 text-sm bg-purple-100 text-purple-700 rounded hover:bg-purple-200 transition-colors disabled:opacity-50">
+          <Sparkles className="size-4"> </Sparkles>
+          AI Enhance
         </button>
       </div>
       <div className="mt-6">
-        <textarea value={data ||""} onChange={(e)=>onChange(e.target.value)} rows={7} className='w-full p-3 px-4 border text-sm border-gray-300 rounded-lg focus:ring focus:ring-blue-500 focus:border-blue-500 outline-none transition-colors resize-none' placeholder='Write a professional summmary that highlights your key strengths and career objectives.'/>
-        <p>Tip: keep it concise (3-4 sentences) and focus on your most relevant achievements and ksills</p>
+        <textarea
+          value={data || ""}
+          onChange={(e) => onChange(e.target.value)}
+          rows={7}
+          className="w-full p-3 px-4 border text-sm border-gray-300 rounded-lg focus:ring focus:ring-blue-500 focus:border-blue-500 outline-none transition-colors resize-none"
+          placeholder="Write a professional summmary that highlights your key strengths and career objectives."
+        />
+        <p>
+          Tip: keep it concise (3-4 sentences) and focus on your most relevant
+          achievements and skills
+        </p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProfessionalSummaryForm
+export default ProfessionalSummaryForm;
